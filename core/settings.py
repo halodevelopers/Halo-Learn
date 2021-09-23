36d921dde2106f723ffb2a4ded37198343e7ef30
kuyesu <2019bce028@std.must.ac.ug>
2021-09-23 19:01:56 +0100

    heroku database settings

--- core/settings.py
+++ core/settings.py
@@ -17,6 +17,11 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# for heroku settings
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
